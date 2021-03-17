32c7b9f295e4c515828b029880818cdfffa24204
Yousuf <aarajyousuf.qtec@gmail.com>
2021-03-17 15:13:04 +0600

    successfully integrate pull to refresh

--- app/src/main/java/com/example/pull_to_refresh/MainActivity.kt
+++ app/src/main/java/com/example/pull_to_refresh/MainActivity.kt
@@ -2,22 +2,29 @@ package com.example.pull_to_refresh
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
 import com.example.pull_to_refresh.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+
+    lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    lateinit var textView: TextView
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =  ActivityMainBinding.inflate(layoutInflater)
-        binding.tv.text = "Refreshing..."
+        setContentView(R.layout.activity_main)
+        swipeRefreshLayout = findViewById(R.id.swipe_container)
+        textView = findViewById(R.id.tv)
+        textView.text = "Refreshing..."
 
-        binding.swipeContainer.setOnRefreshListener {
-            binding.tv.text = "Refreshed"
+        swipeRefreshLayout.setOnRefreshListener {
+            textView.text = "Refreshed"
+            swipeRefreshLayout.isRefreshing = false
         }
     }
 }
